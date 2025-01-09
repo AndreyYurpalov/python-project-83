aecdde88be048af26ca7bb0b665cbe2d1676dd83
Andrey Yurpalov <a.yurpalov@mail.ru>
2025-01-09 21:00:59 +0500

    Added modifided files

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,15 @@
 from flask import Flask, request
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return 'Hello, World! It is my first progect'
