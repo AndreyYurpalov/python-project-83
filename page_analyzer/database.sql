4cc31dd8972cfe8c51dab94a45c3c870eefcf305
Andrey Yurpalov <a.yurpalov@mail.ru>
2025-01-20 11:42:05 +0500

    Modified database.sql

--- page_analyzer/database.sql
+++ page_analyzer/database.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS new_urls
+CREATE TABLE IF NOT EXISTS urls
 (
     id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
     name character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    created_at date NOT NULL,
+    created_at date,
     CONSTRAINT new_name_pkey PRIMARY KEY (id)
 )
 
