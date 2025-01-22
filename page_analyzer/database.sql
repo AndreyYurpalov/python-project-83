61638a2f587dc5e9aa8ca6b89d121c1d35ff1e94
Andrey Yurpalov <a.yurpalov@mail.ru>
2025-01-22 18:16:07 +0500

    Modified database.sql

--- page_analyzer/database.sql
+++ page_analyzer/database.sql
@@ -1,8 +1,20 @@
-CREATE TABLE IF NOT EXISTS public.urls
+"CREATE TABLE urls
 (
     id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
-    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    name character varying(255) NOT NULL,
     created_at date,
     CONSTRAINT urls_pkey PRIMARY KEY (id)
-)
+);"
+
+"CREATE TABLE url_checks
+(
+    id bigint NOT NULL,
+    url_id bigint,
+    status_code bigint,
+    h1 text,
+    title text,
+    description text,
+    created_at date NOT NULL,
+    CONSTRAINT url_checks_pkey PRIMARY KEY (id)
+);"
 
