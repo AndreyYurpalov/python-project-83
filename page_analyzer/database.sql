9a2323822a251c3ddd50f48a57942719adcbea21
Andrey Yurpalov <a.yurpalov@mail.ru>
2025-01-24 15:53:18 +0500

    Modified database.sql

--- page_analyzer/database.sql
+++ page_analyzer/database.sql
@@ -1,21 +1,23 @@
-CREATE TABLE public.urls
+CREATE TABLE IF NOT EXISTS public.urls
 (
-    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name character varying(255) NOT NULL,
-    created_at date,
-    CONSTRAINT urls_pkey PRIMARY KEY (id)
-)
+    created_at date
+);
 
 
-CREATE TABLE public.url_checks
+CREATE TABLE IF NOT EXISTS public.url_checks
 (
     id bigint NOT NULL,
-    url_id bigint,
-    status_code bigint,
+    url_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    status_code smallint,
     h1 text,
     title text,
     description text,
-    created_at date NOT NULL,
-    CONSTRAINT url_checks_pkey PRIMARY KEY (id)
+    created_at date,
+    CONSTRAINT url_checks_pkey PRIMARY KEY (url_id),
+    CONSTRAINT id FOREIGN KEY (id)
+        REFERENCES public.urls (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 )
-
