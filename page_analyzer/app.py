ff4563baca5d92e2576efceba7c7e6380c7e0d23
Andrey Yurpalov <a.yurpalov@mail.ru>
2025-02-09 11:35:09 +0500

    Modified app.py db_function.py functions.py index.html new.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,8 @@
 import os
 
+import psycopg2
 import requests
 import validators
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from flask import (
     Flask,
@@ -17,7 +17,6 @@ from flask import (
 from page_analyzer.db_function import (
     get_data,
     get_data_check,
-    get_domain,
     get_id,
     get_id_name_created_at,
     get_max_date,
@@ -25,12 +24,18 @@ from page_analyzer.db_function import (
     insert_name_url,
     is_url,
 )
+from page_analyzer.functions import (
+    get_domain,
+    url_parser,
+)
 
 load_dotenv()
 
+DATABASE_URL = os.getenv('DATABASE_URL')
+connection = psycopg2.connect(DATABASE_URL)
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-DATABASE_URL = os.getenv('DATABASE_URL')
 
 
 @app.route('/')
@@ -41,15 +46,16 @@ def index():
 
 @app.route('/urls')
 def get_sites():
-    sites = get_data()
-    table = get_max_date()
-    messages = get_flashed_messages(with_categories=True)
-    return render_template(
-        'show.html',
-        sites=sites,
-        table=table,
-        messages=messages,
-    )
+    with connection as con:
+        sites = get_data(con)
+        table = get_max_date(con)
+        messages = get_flashed_messages(with_categories=True)
+        return render_template(
+            'show.html',
+            sites=sites,
+            table=table,
+            messages=messages,
+        )
 
 
 @app.route('/urls', methods=['POST'])
@@ -58,14 +64,15 @@ def get_site():
     url_length = len(url)
     url = get_domain(url)
     if validators.url(url) and url_length <= 255:
-        if int(is_url(url)):
-            flash('Страница уже существует', 'info')
-            id = get_id(url)[0]
-        else:
-            insert_name_url(url)
-            flash('Страница успешно добавлена', 'success')
-            id = get_id(url)[0]
-        return redirect(url_for('get_site_information', id=id))
+        with connection as con:
+            if int(is_url(url, con)):
+                flash('Страница уже существует', 'info')
+                id = get_id(url, con)[0]
+            else:
+                insert_name_url(url, con)
+                flash('Страница успешно добавлена', 'success')
+                id = get_id(url, con)[0]
+            return redirect(url_for('get_site_information', id=id))
     else:
         flash('Некорректный URL', 'danger')
         messages = get_flashed_messages(with_categories=True)
@@ -76,50 +83,46 @@ def get_site():
 @app.route('/urls/<int:id>')
 def get_site_information(id):
     messages = get_flashed_messages(with_categories=True)
-    data = get_id_name_created_at(id)
-    if data:
-        id, url, time = data[0], data[1], data[2]
-        table = get_data_check(id)
-        return render_template(
-            'new.html',
-            id=id,
-            url=url,
-            time=time,
-            messages=messages,
-            table=table,
-        )
-    return render_template('nopage.html'), 404
+    with connection as con:
+        data = get_id_name_created_at(id, con)
+        if data:
+            id, url, time = data[0], data[1], data[2]
+            table = get_data_check(id, con)
+            return render_template(
+                'new.html',
+                id=id,
+                url=url,
+                time=time,
+                messages=messages,
+                table=table,
+            )
+        return render_template('nopage.html'), 404
 
 
 @app.route('/urls/<int:id>/checks', methods=['POST'])
 def get_check_site(id):
-    url = get_id_name_created_at(id)[1]
-    try:
-        response = requests.get(f'{url}')
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
-        h1 = soup.find('h1').text if soup.find('h1') else ''
-        title = soup.find('title').text if soup.find('title') else ''
-        description = ''
-        meta_tags = soup.find_all('meta')
-        for tag in meta_tags:
-            if tag.get('name', '').lower() == 'description':
-                description = tag.get('content', '')
-                break
-        status_code = response.status_code
-        insert_check_data_with_id_site(id, status_code, h1, title, description)
-        flash('Страница успешно проверена', 'success')
-        return redirect(url_for('get_site_information', id=id, ))
-    except requests.exceptions.RequestException:
-        flash('Произошла ошибка при проверке', 'danger')
-        return redirect(url_for('get_site_information', id=id, ))
-    except Exception:
-        flash('Произошла ошибка при проверке', 'danger')
-        return redirect(url_for('get_site_information', id=id, ))
+    with connection as con:
+        url = get_id_name_created_at(id, con)[1]
+        try:
+            response = requests.get(f'{url}')
+            response.raise_for_status()
+            url_parse = url_parser(response)
+
+            """   url_parse:   status_code, h1, title, description    """
+
+            insert_check_data_with_id_site(id, con, *url_parse)
+            flash('Страница успешно проверена', 'success')
+            return redirect(url_for('get_site_information', id=id, ))
+        except requests.exceptions.RequestException:
+            flash('Произошла ошибка при проверке', 'danger')
+            return redirect(url_for('get_site_information', id=id, ))
+        except Exception:
+            flash('Произошла ошибка при проверке', 'danger')
+            return redirect(url_for('get_site_information', id=id, ))
 
 
 @app.errorhandler(404)
-def no_page(error):
+def no_page():
     return render_template('nopage.html'), 404
 
 
--- page_analyzer/db_function.py
+++ page_analyzer/db_function.py
@@ -1,76 +1,52 @@
-import os
-from urllib.parse import urlparse
-
-import psycopg2
-from dotenv import load_dotenv
 from psycopg2.extras import NamedTupleCursor
 
-load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL')
 
-
-def get_domain(url: str) -> str:
-    domain = '://'.join([urlparse(url).scheme, urlparse(url).netloc])
-    return domain
-
-
-def insert_name_url(name):
-    connection = psycopg2.connect(DATABASE_URL)
+def insert_name_url(name, connection):
     with connection.cursor() as curs:
         sql_name = """INSERT INTO public.urls 
                                 (name) VALUES (%s);"""
         curs.execute(sql_name, (name,))
         connection.commit()
-        connection.close()
 
 
-def get_data():
-    connection = psycopg2.connect(DATABASE_URL)
+def get_data(connection):
     with connection.cursor() as curs:
         sql_select = "SELECT * FROM public.urls ORDER BY id DESC;"
         curs.execute(sql_select)
         result = curs.fetchall()
         connection.commit()
-        connection.close()
         return result
 
 
-def get_id(url):
-    connection = psycopg2.connect(DATABASE_URL)
+def get_id(url, connection):
     with connection.cursor() as curs:
         sql_url = "SELECT id FROM public.urls WHERE name = %s;"
         curs.execute(sql_url, (url,))
         result = curs.fetchone()
         connection.commit()
-        connection.close()
         return result
 
 
-def get_id_name_created_at(_id):
-    connection = psycopg2.connect(DATABASE_URL)
+def get_id_name_created_at(_id, connection):
     with connection.cursor() as curs:
         sql_id = "SELECT * FROM public.urls WHERE id = %s;"
         curs.execute(sql_id, (_id,))
         result = curs.fetchone()
         connection.commit()
-        connection.close()
         return result
 
 
-def get_data_check(_id):
-    connection = psycopg2.connect(DATABASE_URL)
+def get_data_check(_id, connection):
     with connection.cursor(cursor_factory=NamedTupleCursor) as curs:
         sql_id = """SELECT * FROM public.url_checks 
                       WHERE id = %s ORDER BY url_id DESC;"""
         curs.execute(sql_id, (_id,))
         result = curs.fetchall()
         connection.commit()
-        connection.close()
         return result
 
 
-def get_max_date():
-    connection = psycopg2.connect(DATABASE_URL)
+def get_max_date(connection):
     with connection.cursor() as curs:
         sql_select = """SELECT id, status_code, MAX(created_at)
                        AS max_created_at FROM public.url_checks 
@@ -78,12 +54,10 @@ def get_max_date():
         curs.execute(sql_select)
         result = curs.fetchall()
         connection.commit()
-        connection.close()
         return result
 
 
-def is_url(url):
-    connection = psycopg2.connect(DATABASE_URL)
+def is_url(url, connection):
     with connection.cursor() as curs:
         sql_url = """SELECT CAST
                      (CASE WHEN EXISTS
@@ -92,12 +66,11 @@ def is_url(url):
         curs.execute(sql_url, (url,))
         result = curs.fetchone()[0]
         connection.commit()
-        connection.close()
         return result
 
 
-def insert_check_data_with_id_site(_id, status_code, h1, title, description):
-    connection = psycopg2.connect(DATABASE_URL)
+def insert_check_data_with_id_site(_id, connection, status_code, h1,
+                                   title, description):
     with connection.cursor() as curs:
         sql_all_data = """INSERT INTO public.url_checks 
                           (id, url_id, status_code, h1, title, 
@@ -106,4 +79,3 @@ def insert_check_data_with_id_site(_id, status_code, h1, title, description):
         curs.execute(sql_all_data,
                      (_id, status_code, h1, title, description,))
         connection.commit()
-        connection.close()
